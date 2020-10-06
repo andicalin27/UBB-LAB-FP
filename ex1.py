c6694edfd175b4ef44dd21192bed89b02225c28a
Mihnea <mihneaaleman@yahoo.com>
2020-10-06 14:58:31 +0300

    second commit

--- ex1.py
+++ ex1.py
@@ -4,10 +4,7 @@ x=1
 
 def produs(nr):
     global p
-    if nr==0:
-
-         return p
-    else:
+    if nr!=0:
         p=p*nr
 
 
@@ -16,6 +13,13 @@ while x!=0:
     produs(x)
     x=int(input())
 
-print(produs(x))
+print(p)
+ct=0
+while p>0:
+    if p%10==0:
+        ct+=1
+    p=p/10
+print(ct)
+
 
 
